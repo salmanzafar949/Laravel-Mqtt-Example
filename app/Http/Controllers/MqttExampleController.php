be80b9e7ceed08fd7a6389ac167c57a83ae967d3
Salman Zafar <salman.zafar@innowi.com>
2019-06-14 13:20:59 +0500

    Added publsihn topic and message to mqtt

--- app/Http/Controllers/MqttExampleController.php
+++ app/Http/Controllers/MqttExampleController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Mqtt;
 
 class MqttExampleController extends Controller
 {
-    //
+
+    // publishing topic
+
+    public function SendMsgViaMqtt($topic, $message)
+    {
+        $output = Mqtt::ConnectAndPublish($topic, $message);
+
+        if ($output === true)
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/mqtt/publish/{topic}/{message}', 'MqttExampleController@SendMsgViaMqtt');
